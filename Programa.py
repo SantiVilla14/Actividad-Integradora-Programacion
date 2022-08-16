01bdfee48f26f422c94617538fd5f8b4a7725cb7
Santiago <107277803+SantiVilla14@users.noreply.github.com>
2022-08-15 23:05:08 -0300

    Update Programa.py

--- Programa.py
+++ Programa.py
@@ -15,6 +15,7 @@ print("Que quieres hacer con estos numeros: ", numeros)
 print()
 print("[1 - Función Suma] [2 - Función Promedio] [3 - Función Máximo] [4 - Función Mínimo] [Cualquier otro - Salir]")
 i=int(input())
+print()
 
 if i==1:
     def sumar(numeros):
@@ -27,18 +28,39 @@ if i==1:
     print(sumar(numeros))
 
 elif i==2:
-    suma = numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4]
-    promedio = float(suma // 5)
-    print("El promedio de tus numeros es:")
-    print(promedio)
+    def promedio():
+        suma = numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4]
+        prom = float(suma // 5)
+        print("El promedio de tus numeros es:")
+        print(prom)
+    promedio()
 
 elif i==3:
-    print("El maximo de tus numeros es: ")
-    print(max(numeros))
+    def mayor(numeros):
+        mayor=None
+        for nro in numeros:
+            if mayor==None:
+                mayor=nro
+            else:
+                if nro>mayor:
+                    mayor=nro
+        print("El maximo de tus numeros es: ")
+        print(mayor)
+    mayor(numeros)
 
 elif i==4:
-        print("El minimo de tus numeros es: ")
-        print(min(numeros))
+        def menor(numeros):
+            menor=None
+            for nro in numeros:
+                if menor==None:
+                    menor=nro
+                else:
+                    if nro<menor:
+                        menor=nro
+            print("El minimo de tus numeros es: ")
+            print(menor)
+        menor(numeros)
+            
 else:
     print()
     print("Saliendo...")
